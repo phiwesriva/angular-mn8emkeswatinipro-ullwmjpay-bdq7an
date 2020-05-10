f7228b23deae891714e78f17b264313247db9cc3
phiwesriva <phiwes@icloud.com>
2020-05-10 15:14:32 +0200

    2 files modified

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -25,8 +25,40 @@ contactForm;
 
     });}
 
-  ngOnInit() {
+  ngOnInit(): void {
+  this.contactForm = new FormGroup({
+    'nameandsurname': new FormControl(this.contact.nameandsurname, [
+      Validators.required,
+      Validators.minLength(4)
+     // <!-- forbiddenNameValidator(/bob/i)  Here's how you pass in the custom validator.
+    ]),
+
+    'Cname': new FormControl(this.contact.Cname),
+
+    'Cnumber': new FormControl(this.contact.Cnumber, [
+      Validators.required,
+      Validators.minLength(4)
+     // <!-- forbiddenNameValidator(/bob/i)  Here's how you pass in the custom validator.
+    ]),
+    
+    'Eaddress': new FormControl(this.contact.Eaddress),
+    'areaofinterest': new FormControl(this.contact.areaofinterest, Validators.required),
+       
+
+    'Info': new FormControl(this.contact.Info, [
+      Validators.required,
+      Validators.minLength(4)
+     // <!-- forbiddenNameValidator(/bob/i)  Here's how you pass in the custom validator.
+    ])
+    
+  });
   }
+
+  
+get nameandsurname() { return this.contact.get('nameandsurname'); }
+get Cnumber() { return this.contact.get('Cnumber'); }
+get areaofinterest() { return this.contact.get('areaofinterest'); }
+get Info() { return this.contact.get('Info'); }
 
 onSubmit(customerData) {
     // Process checkout data here

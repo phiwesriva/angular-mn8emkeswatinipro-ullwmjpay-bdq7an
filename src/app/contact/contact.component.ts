bea56849bc02d422ffbc8ec8c06a587f838d38e9
phiwesriva <phiwes@icloud.com>
2020-05-11 09:42:50 +0200

    2 files modified

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,64 +1,71 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
 import { FormControl } from '@angular/forms';
-/*import { Validators } from '@angular/forms';*/
+import { Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.css']
 })
+
+
 export class ContactComponent implements OnInit {
 contactForm;
 
-  constructor(
+constructor(
     private formBuilder: FormBuilder,) { 
     this.contactForm = this.formBuilder.group({
 
-      nameandsurname: '',
+      nameandsurname: ['', Validators.required],
       Cname: '',
-      Cnumber: '',
+      Cnumber: ['', Validators.required],
       Eaddress: '',
-      areaofinterest: '',
-      Info: ''
+      areaofinterest: ['', Validators.required],
+      Info: ['', Validators.required]
 
 
     });}
 
-  ngOnInit(): void {
-  this.contactForm = new FormGroup({
-    'nameandsurname': new FormControl(this.contact.nameandsurname, [
-      Validators.required,
-      Validators.minLength(4)
-     // <!-- forbiddenNameValidator(/bob/i)  Here's how you pass in the custom validator.
-    ]),
-
-    'Cname': new FormControl(this.contact.Cname),
-
-    'Cnumber': new FormControl(this.contact.Cnumber, [
-      Validators.required,
-      Validators.minLength(4)
-     // <!-- forbiddenNameValidator(/bob/i)  Here's how you pass in the custom validator.
-    ]),
+  ngOnInit(){
     
-    'Eaddress': new FormControl(this.contact.Eaddress),
-    'areaofinterest': new FormControl(this.contact.areaofinterest, Validators.required),
+  }
+  //: void {
+  //this.contactForm = new FormGroup({
+    //'nameandsurname': new FormControl(this.contact.nameandsurname, [
+      //Validators.required,
+      //Validators.minLength(4)
+      //forbiddenNameValidator(/bob/i)  Here's how you pass in the custom validator.
+    //]),
+
+    //'Cname': new FormControl(this.contact.Cname),
+
+    //'Cnumber': new FormControl(this.contact.Cnumber, [
+      //Validators.required,
+      //Validators.minLength(4)
+     // <!-- forbiddenNameValidator(/bob/i)  Here's how you pass in the custom validator.
+    //]),
+    
+    //'Eaddress': new FormControl(this.contact.Eaddress),
+    //'areaofinterest': new FormControl(this.contact.areaofinterest, Validators.required),
        
 
-    'Info': new FormControl(this.contact.Info, [
-      Validators.required,
-      Validators.minLength(4)
+    //'Info': new FormControl(this.contact.Info, [
+      //Validators.required,
+      //Validators.minLength(4)
      // <!-- forbiddenNameValidator(/bob/i)  Here's how you pass in the custom validator.
-    ])
+    //])
     
-  });
-  }
+  //});
+  //}
 
   
-get nameandsurname() { return this.contact.get('nameandsurname'); }
-get Cnumber() { return this.contact.get('Cnumber'); }
-get areaofinterest() { return this.contact.get('areaofinterest'); }
-get Info() { return this.contact.get('Info'); }
+//get nameandsurname() { return this.contact.get('nameandsurname'); }
+//get Cnumber() { return this.contact.get('Cnumber'); }
+//get areaofinterest() { return this.contact.get('areaofinterest'); }
+//get Info() { return this.contact.get('Info'); }
+//
 
 onSubmit(customerData) {
     // Process checkout data here
